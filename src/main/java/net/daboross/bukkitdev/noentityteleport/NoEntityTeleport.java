259cbb6d0fea55389d9ea1a8816d9cededd4d228
Dabo Ross <daboross@daboross.net>
2013-07-21 00:41:23 -0700

    Created

--- src/main/java/net/daboross/bukkitdev/noentityteleport/NoEntityTeleport.java
+++ src/main/java/net/daboross/bukkitdev/noentityteleport/NoEntityTeleport.java
@@ -18,7 +18,10 @@ package net.daboross.bukkitdev.noentityteleport;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPortalEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -28,22 +31,24 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class NoEntityTeleport extends JavaPlugin implements Listener {
 
-    @Override
-    public void onEnable() {
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(this, this);
-    }
+	@Override
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
+	}
 
-    @Override
-    public void onDisable() {
-    }
+	@Override
+	public void onDisable() {
+	}
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("")) {
-        } else {
-            sender.sendMessage("NoEntityTeleport doesn't know about the command /" + cmd);
-        }
-        return true;
-    }
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		sender.sendMessage("NoEntityTeleport doesn't know about the command /" + cmd);
+		return true;
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onEntityTeleport(EntityPortalEvent epe) {
+		epe.setCancelled(true);
+	}
 }
